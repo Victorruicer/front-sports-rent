665b7102a9ffc1f514f53455cf3462b2690d17d0
eduo <eduardoruizconde@hotmail.com>
2020-12-13 19:10:28 +0100

    seguimos con reservas

--- src/app/components/reservas/card-reserva/card-reserva.component.ts
+++ src/app/components/reservas/card-reserva/card-reserva.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { PistaReservaModel } from '../models/pistaReservaModel';
 import { AppState } from '../../../app.reducer';
@@ -18,6 +18,8 @@ export class CardReservaComponent implements OnInit {
   datosPista: PistaReservaModel;
   precioFinal: number;
   totalHoras: number;
+  cambia = true;
+
 
   constructor(private fb: FormBuilder, private store: Store<AppState>) {
     this.crearFormulario();
@@ -30,35 +32,86 @@ export class CardReservaComponent implements OnInit {
           pista => {
             if(pista.Pista == this.pista){
               this.datosPista = pista;
-              this.horarios = this.creaSelectHorario(pista.LibresReservadas);
+              this.horarios = this.creaSelectHorario(this.datosPista.LibresReservadas, this.cambia);
             }
           })
         })
+    }
+
+    //al cambiar la seleccion del radio cambiamos los valores del select que se mostrará
+    cambiaHora(evt) {
+      this.cambia = (evt.target.value == "option1") ? true : false;
+      this.horarios = this.creaSelectHorario(this.datosPista.LibresReservadas, this.cambia);
     }
 
   crearFormulario(){
 
     this.formulario = this.fb.group({
       horaUno: '',
-      horaDos: ''
+      horaDos: '',
+      radio: ['', Validators.required]
     });
   }
 
   limpiar(){}
 
-  creaSelectHorario(horas: string[]){
+  creaSelectHorario(horas: string[], opcion: boolean){
     var ini = 8;
     var selecthora = [];
-    for(var x = 0; x < horas.length; x++){
-      if(horas[x] == "libre"){
-        var newhora = (ini < 10) ? "0"+ini+":00:00" : ini+":00:00";
-        selecthora.push(newhora);
+      console.log("las horas "+horas)
+      for(var x = 0; x < horas.length; x++){
+        if(horas[x] == "libre"){
+          switch(x){
+/*             case (0):
+              if(opcion){
+                selecthora.push(this.transformaHora(ini, opcion));
+              }else{
+                if(horas[x+1] != 'reservada'){
+                selecthora.push(this.transformaHora(ini, opcion));
+                }
+              }
+              break; */
+            case (horas.length -2):
+              if(opcion){
+                selecthora.push(this.transformaHora(ini, opcion));
+              }
+              break;
+            default:
+              if(opcion){
+                selecthora.push(this.transformaHora(ini, opcion));
+              }else{
+                if(horas[x+1] != 'reservada'){
+                selecthora.push(this.transformaHora(ini, opcion));
+                }
+              }
+              break;
+          }
+        }
+/*         if(horas[x] == "libre"){
+
+          var hini = (ini < 10) ? "0"+ini+":00" : ini+":00";
+          var hfin = (ini+1 < 10) ? "0"+(ini+1)+":00" : (ini+1)+":00";
+
+          var newhora = "de "+hini+" a "+hfin;
+          selecthora.push(newhora);
+        }*/
+        ini++;
       }
-      ini++;
-    }
     console.log("horas libres = "+ selecthora);
     return selecthora;
   }
+
+    transformaHora(ini: number, opcion: boolean) {
+      if(opcion){
+      var hini = (ini < 10) ? "0"+ini+":00" : ini+":00";
+      var hfin = (ini+1 < 10) ? "0"+(ini+1)+":00" : (ini+1)+":00";
+      }else{
+      var hini = (ini < 10) ? "0"+ini+":00" : ini+":00";
+      var hfin = (ini+2 < 10) ? "0"+(ini+1)+":00" : (ini+2)+":00";
+      }
+
+      return "de "+hini+" a "+hfin;
+    }
 
   reservar(){
 
